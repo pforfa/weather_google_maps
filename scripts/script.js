f48e1eaec18561276b216ab5c058266843e26b54
David <drakexp@gmail.com>
2016-06-10 15:00:05 -0400

    changes to google maps

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
-// This example adds a search box to a map, using the Google Place Autocomplete
-// feature. People can enter geographical searches. The search box will return a
-// pick list containing a mix of places and predicted search terms.
+$(document).ready(function(){
+  $("#search").on("submit",function(e){
+    e.preventDefault();
+    var formData = {
+      'sinput'              : $('input[name=sinput]').val()
+    };
 
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+  });
+});
+
 
 function initAutocomplete() {
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -14,9 +17,9 @@ function initAutocomplete() {
   });
 
   // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
+  var input = sinput;
   var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
   // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
@@ -68,3 +71,7 @@ function initAutocomplete() {
     map.fitBounds(bounds);
   });
 }
+
+// function search() {
+//   var searchval = 
+// }
